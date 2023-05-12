e8b4b52b39fbfefe4936a6bd6dfc60a5a551ba38
Jerikho Silaban <jerikho2622@gmail.com>
2023-05-12 16:45:48 +0700

    [Update] Added GetProducts and DeleteProduct functions

--- controllers/productControllers.go
+++ controllers/productControllers.go
@@ -20,6 +20,16 @@ func CreateProduct(ctx *gin.Context) {
 
 	Product := models.Product{}
 	userID := uint(userData["id"].(float64))
+	User := models.User{}
+	errA := db.First(&User, "id = ?", userID).Error
+	if errA != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"error":   "User Not Found",
+			"message": errA.Error(),
+		})
+
+		return
+	}
 
 	if contentType == appJSON {
 		ctx.ShouldBindJSON(&Product)
@@ -28,6 +38,7 @@ func CreateProduct(ctx *gin.Context) {
 	}
 
 	Product.UserID = userID
+	Product.User = &User
 
 	err := db.Debug().Create(&Product).Error
 
@@ -40,7 +51,9 @@ func CreateProduct(ctx *gin.Context) {
 		return
 	}
 
-	ctx.JSON(http.StatusCreated, Product)
+	ctx.JSON(http.StatusCreated, gin.H{
+		"data": Product,
+	})
 }
 
 func UpdateProduct(ctx *gin.Context) {
@@ -71,28 +84,18 @@ func UpdateProduct(ctx *gin.Context) {
 		return
 	}
 
-	ctx.JSON(http.StatusOK, Product)
+	ctx.JSON(http.StatusOK, gin.H{
+		"data": Product,
+	})
 }
 
 func GetProductById(ctx *gin.Context) {
 	db := database.GetDB()
-	userData := ctx.MustGet("userData").(jwt.MapClaims)
-	contentType := helpers.GetContentType(ctx)
+
 	Product := models.Product{}
-
 	productID, _ := strconv.Atoi(ctx.Param("productID"))
-	userID := uint(userData["id"].(float64))
 
-	if contentType == appJSON {
-		ctx.ShouldBindJSON(&Product)
-	} else {
-		ctx.ShouldBind(&Product)
-	}
-
-	Product.UserID = userID
-	Product.ID = uint(productID)
-
-	err := db.Debug().First(&Product, "id = ?", Product.ID).Error
+	err := db.Debug().Preload("User").First(&Product, "id = ?", uint(productID)).Error
 	if err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			ctx.AbortWithStatusJSON(http.StatusNotFound, gin.H{
@@ -107,5 +110,48 @@ func GetProductById(ctx *gin.Context) {
 		return
 	}
 
-	ctx.JSON(http.StatusOK, Product)
+	ctx.JSON(http.StatusOK, gin.H{
+		"data": Product,
+	})
+}
+
+func GetProducts(ctx *gin.Context) {
+	db := database.GetDB()
+
+	Products := []models.Product{}
+
+	err := db.Debug().Find(&Products).Error
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error":   "Bad Request",
+			"message": err.Error(),
+		})
+
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"data": Products,
+	})
+}
+
+func DeleteProduct(ctx *gin.Context) {
+	db := database.GetDB()
+
+	Product := models.Product{}
+	productID, _ := strconv.Atoi(ctx.Param("productID"))
+
+	err := db.Debug().Where("id = ?", uint(productID)).Delete(&Product).Error
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error":   "Bad Request",
+			"message": err.Error(),
+		})
+
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "Successfully deleted product",
+	})
 }
